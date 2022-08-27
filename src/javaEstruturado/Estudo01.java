2fd52627b13ce5be3a910bc3750fbdfafd758e5d
SamuelBelloSB <samuelbellocontato@gmail.com>
2022-08-27 19:01:48 -0300

    estudando sobre as variáveis Java, variáveis tipo inteiro

---
+++ src/javaEstruturado/Estudo01.java
@@ -0,0 +1,19 @@
+package javaEstruturado;
+
+public class Estudo01 {
+    public static void main(String[] args) throws Exception {
+        //tipo nome
+
+        // variáveis do tipo inteiro
+        byte numeroByte = 127;
+        short numeroShort = 32767;
+        int numeroInt = 2147483647;
+        long numeroLong = 922337203685477000l;
+        System.out.println(numeroByte);
+        System.out.println(numeroShort);
+        System.out.println(numeroInt);
+        System.out.println(numeroLong);
+
+        // variáveis do tipo flutuante
+    }
+}
