0fbb8da1cbc23766fc516e56f162cc36e92acb8b
SamuelBelloSB <samuelbellocontato@gmail.com>
2022-08-27 19:18:22 -0300

    Variáveis flutuantes

--- src/javaEstruturado/Estudo01.java
+++ src/javaEstruturado/Estudo01.java
@@ -4,16 +4,23 @@ public class Estudo01 {
     public static void main(String[] args) throws Exception {
         //tipo nome
 
-        // variáveis do tipo inteiro
+        // Variáveis do tipo inteiro
+        System.out.println("Variáveis do tipo inteiro:");
         byte numeroByte = 127;
         short numeroShort = 32767;
         int numeroInt = 2147483647;
-        long numeroLong = 922337203685477000l;
+        long numeroLong = 922337203685477000l; //Número do tipo Long precisam da letra 'l' no final.
         System.out.println(numeroByte);
         System.out.println(numeroShort);
         System.out.println(numeroInt);
         System.out.println(numeroLong);
 
-        // variáveis do tipo flutuante
+        // Variáveis do tipo flutuante
+        System.out.println("Variáveis do tipo flutuante:");
+        float peso = 3.2f; //Número do tipo Float precisa da letra 'f' no final.
+        double pi = 3.1415926535897932384626433832795;
+        System.out.print(peso);
+        System.out.print(pi);
+
     }
 }
